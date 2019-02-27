79ca7cbd2c9ab615da81bf58b5833f831f7b2259
Kinfu <ykinfu@deloitte.com>
2019-02-27 16:39:13 -0500

    Change on status

--- LightningCardGoal/force-app/main/default/aura/LightningCard/LightningCardController.js
+++ LightningCardGoal/force-app/main/default/aura/LightningCard/LightningCardController.js
@@ -1,12 +1,12 @@
 ({
 	doInit : function(component, event, helper) {
 		
-        var opts = [
+       /* var opts = [
               { "class": "optionClass", label: "Initiated", value: "opt1", selected: "true" },
               { "class": "optionClass", label: "Pending", value: "opt2" },
               { "class": "optionClass", label: "Completed", value: "opt3" }
               
           ];
-        component.find("InputSelectDynamic").set("v.options", opts);
-	}
+        component.find("InputSelectDynamic").set("v.options", opts);*/
+	} 
 })
